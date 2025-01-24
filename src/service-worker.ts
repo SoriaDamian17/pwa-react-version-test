8458b5db15069b204a6ca0846c0d61192f365a42
Damian Soria <damian.soria@engworks.com>
2025-01-24 14:24:38 -0300

    fix: update version

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -13,8 +13,10 @@ import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
-
+const pkVersion = require('../package.json');
 declare const self: ServiceWorkerGlobalScope;
+
+const version = pkVersion.version;
 
 clientsClaim();
 
