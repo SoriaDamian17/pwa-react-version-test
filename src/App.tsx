354d02e152df9e8c7b7a14229d9754e47905e435
Damian Soria <damian.soria@engworks.com>
2025-01-24 15:46:19 -0300

    fix: update version

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Snackbar, Alert } from '@mui/material';
+import { register } from './serviceWorkerRegistration';
 const pkVersion = require('../package.json');
 
 function App() {
+
+  const [updateAvailable, setUpdateAvailable] = useState(true);
+
+  useEffect(() => {
+    // Registrar el Service Worker y manejar el evento onUpdate
+    register({
+      onUpdate: (registration: any) => {
+        const { waiting } = registration || {};
+
+        if (waiting) {
+          setUpdateAvailable(true); // Mostrar la alerta cuando hay una nueva versión
+
+          // Opción de forzar el "skipWaiting" cuando el usuario lo permita
+          waiting.postMessage({ type: 'SKIP_WAITING' });
+
+          waiting.addEventListener('statechange', (event: any) => {
+            if (event.target.state === 'activated') {
+              window.location.reload(); // Recargar la página después de la activación
+            }
+          });
+        }
+      },
+    });
+  }, []);
+
+  const handleClose = () => {
+    setUpdateAvailable(false); // Cerrar la alerta
+  };
 
   return (
     <div id="app-desktop">
         <h2>Pwa Test Version {pkVersion.version}.</h2>
         <h3>Update to new version</h3>
+        <Snackbar
+        open={updateAvailable}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        autoHideDuration={null} // Mantener visible hasta que el usuario tome acción
+      >
+        <Alert
+          onClose={handleClose}
+          severity="info"
+          sx={{ width: '100%' }}
+          action={
+            <button
+              onClick={() => window.location.reload()}
+              style={{
+                backgroundColor: '#1976d2',
+                color: 'white',
+                padding: '6px 12px',
+                borderRadius: '4px',
+                border: 'none',
+                cursor: 'pointer',
+                marginLeft: '16px',
+              }}
+            >
+              Actualizar
+            </button>
+          }
+        >
+          Nueva version Disponible!
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -15,32 +15,54 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-  onUpdate: (registration) => {
-    const { waiting } = registration || {};
+// serviceWorkerRegistration.register({
+//   onUpdate: (registration) => {
+//     const { waiting } = registration || {};
 
-    if (waiting) {
-      // Mostrar un mensaje de actualización al usuario
-      const updateBanner = document.createElement('div');
-      updateBanner.innerHTML = `
-        <div style="position: fixed; bottom: 0; width: 100%; background:rgb(28, 134, 72); padding: 16px; text-align: center; z-index: 1000;">
-          A new version is available. <button id="update-now">Update Now</button>
-        </div>
-      `;
-      document.body.appendChild(updateBanner);
+//     if (waiting) {
+//       // Mostrar un mensaje de actualización al usuario
+//       const updateBanner = document.createElement('div');
+//       updateBanner.innerHTML = `
+//         <div style="position: fixed; bottom: 8px; padding: 0 16px; z-index: 1000;">
+          //     <div style="
+          //     background: rgb(28, 134, 72); 
+          //     padding: 8px 16px; 
+          //     border-radius: 16px; 
+          //     text-align: center; 
+          //     display: flex; 
+          //     justify-content: space-around; 
+          //     align-items: center; 
+          //     color: #fff;
+          //     z-index: 1000;">
+          //   <span>Nueva Versión Disponible.</span>
+          //   <button id="update-now" style="
+          //       cursor: pointer; 
+          //       background: transparent; 
+          //       border: 1px solid #fff; 
+          //       border-radius: 4px; 
+          //       padding: 8px 16px; 
+          //       margin-left: 16px; 
+          //       text-transform: uppercase; 
+          //       color: #fff;">
+          //     Actualizar
+          //   </button>
+          // </div>
+          // </div>
+//       `;
+//       document.body.appendChild(updateBanner);
 
-      document.getElementById('update-now')?.addEventListener('click', () => {
-        waiting.postMessage({ type: 'SKIP_WAITING' });
+//       document.getElementById('update-now')?.addEventListener('click', () => {
+//         waiting.postMessage({ type: 'SKIP_WAITING' });
 
-        waiting.addEventListener('statechange', (event: any) => {
-          if (event.target.state === 'activated') {
-            window.location.reload();
-          }
-        });
-      });
-    }
-  },
-});
+//         waiting.addEventListener('statechange', (event: any) => {
+//           if (event.target.state === 'activated') {
+//             window.location.reload();
+//           }
+//         });
+//       });
+//     }
+//   },
+// });
 
 
 // If you want to start measuring performance in your app, pass a function
